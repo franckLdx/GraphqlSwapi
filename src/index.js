a39453fdbfcc397839583d1b695e0476962f9879
FranckLdx <franck.ledoux.pro@gmail.com>
2016-11-11 18:17:04 +0100

    Use my new lib events-to-any-promise

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@
 import bunyan from 'bunyan';
 import http from 'http';
 import { createApp } from './app.js';
+import eventsToAnyPromise from 'events-to-any-promise';
 
 const logger = bunyan.createLogger({name: "Graphql-Swapi"});
 
+function startServer(port, app) {
+	logger.info('Starting HTTP server');
+	const server = http.createServer(app);
+	server.listen(port);
+	return eventsToAnyPromise(server, 'listening').then(() => {
+		logger.info('Server is up and running');
+		return server;
+	});
+}
+
 logger.info('Server initialisation...');
 createApp()
-	.then((app) => {
-		logger.info('Starting HTTP server');
-		const server = http.createServer(app);
-		server.listen(8000, ()=> { logger.info('Server is up and running'); });
-	})
+	.then((app) => startServer(8000, app))
 	.catch((err) => { logger.error(err); });
